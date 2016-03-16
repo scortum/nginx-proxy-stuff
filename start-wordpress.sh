e9aca3d6689aa4025833f04276e3e13cfa2c0dc4
Alex <alex@aeffle.de>
2016-03-16 01:32:50 +0100

    ...

--- start-wordpress.sh
+++ start-wordpress.sh
@@ -6,3 +6,21 @@ docker run -d  \
            -p 8080:80  \
            wordpress
 
+
+echo checkout https://hub.docker.com/_/wordpress/
+
+cat <<EOF
+
+Full command line example:
+
+docker run -e WORDPRESS_DB_PASSWORD=password -d --name wordpress --link wordpressdb:mysql -p 8080:80 -v "/opt/wordpress":/var/www/html -v "/opt/wordpress_config/uploads.ini:/usr/local/etc/php/conf.d/uploads.ini" wordpress
+
+The contents of /opt/wordpress_config/uploads.ini should contain:
+
+file_uploads = On
+memory_limit = 256M
+upload_max_filesize = 256M
+post_max_size = 300M
+max_execution_time = 600
+
+EOF
