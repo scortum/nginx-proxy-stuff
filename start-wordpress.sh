7fc2993e5b71d6ac7ec5bb10a40d34014a99c709
Alex <alex@aeffle.de>
2016-03-16 11:53:22 +0100

    more config

--- start-wordpress.sh
+++ start-wordpress.sh
@@ -1,26 +1,53 @@
 #!/bin/sh -x
 
-docker run -d  \
-           --name wordpress   \
-           --link mariadb:mysql  \
-           -p 8080:80  \
-           wordpress
+BASE_DIR=/data/wordpress-lineli
+
+if [ ! -d ${BASE_DIR} ]; then
+    echo please create:
+    echo ${BASE_DIR}/wordpress
+    echo ${BASE_DIR}/wordpress_config
+    exit -1
+fi
 
 
-echo checkout https://hub.docker.com/_/wordpress/
-
-cat <<EOF
-
-Full command line example:
-
-docker run -e WORDPRESS_DB_PASSWORD=password -d --name wordpress --link wordpressdb:mysql -p 8080:80 -v "/opt/wordpress":/var/www/html -v "/opt/wordpress_config/uploads.ini:/usr/local/etc/php/conf.d/uploads.ini" wordpress
-
-The contents of /opt/wordpress_config/uploads.ini should contain:
-
+cat > /data/wordpress-lineli/wordpress_config/uploads.ini <<EOF
 file_uploads = On
 memory_limit = 256M
 upload_max_filesize = 256M
 post_max_size = 300M
 max_execution_time = 600
+EOF
 
+docker stop wordpress
+docker rm wordpress
+docker run -d  \
+           --name wordpress   \
+           --link mariadb:mysql  \
+           -v ${BASE_DIR}/wordpress:/var/www/html   \
+           -v ${BASE_DIR}/wordpress_config/uploads.ini:/usr/local/etc/php/conf.d/uploads.ini   \
+           -p 8080:80  \
+           wordpress
+
+
+
+cat << EOF
+Checkout https://hub.docker.com/_/wordpress/
+
+Full command line example:
+  docker run -e WORDPRESS_DB_PASSWORD=password  \
+             -d  \
+             --name wordpress \
+             --link wordpressdb:mysql  \
+             -p 8080:80   \
+             -v "/opt/wordpress":/var/www/html   \
+             -v "/opt/wordpress_config/uploads.ini:/usr/local/etc/php/conf.d/uploads.ini"   \
+             wordpress
+
+
+The contents of /opt/wordpress_config/uploads.ini should contain:
+  file_uploads = On
+  memory_limit = 256M
+  upload_max_filesize = 256M
+  post_max_size = 300M
+  max_execution_time = 600
 EOF
